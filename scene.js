2ad3caec687f429729fc74d72065951034f84f51
AishwaryaBhattbhatt <aishwarya.bhattbhatt@cbc.ca>
2025-12-03 13:44:19 -0500

    1

---
+++ scene.js
@@ -0,0 +1,17 @@
+export const scenes = [
+    {
+      id: "scene-01",
+      image: "assets/person-01.png",
+  
+      particles: "assets/dots.png",
+
+      orbits: [
+        { radius: 190, delay: 1 },
+        { radius: 260, delay: 1.5 },
+        { radius: 330, delay: 2 }
+      ],
+  
+        iconsJson: "assets/icons/icon-positions.json"
+    }
+  ];
+  
